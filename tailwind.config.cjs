bcf802898c21b24cb05861c2dcb2f40027ce56fc
Kristina Andersen <kris91r3@stud.kea.dk>
2022-08-25 22:30:21 +0200

    opsætning og mange rettelser

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#C2410C",
+      secondary: "#FECC16",
+      linkfarve: "#137586",
+      darkgrey: "#121212",
+      lightgrey: "#3a3a3a",
+      light: "#dfdfdf",
+      white: "#ffffff",
+      black: "#000000",
+    },
+    extend: {
+      fontFamily: {
+        speciel_elite: "'Special Elite', sans-serif",
+        merriweather: "'Merriweather', serif",
+        allison: "'Allison', serif",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
